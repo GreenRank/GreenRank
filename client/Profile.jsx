22974557094821391d14e2cb942ffc894da21f62
Raubern Totanes <rstotanes@g.ucla.edu>
2021-07-28 21:33:49 -0400

    fixed piechart styling

--- client/Profile.jsx
+++ client/Profile.jsx
@@ -1,6 +1,6 @@
 import { render } from "enzyme";
 import React, { useState, useEffect } from "react";
-import { PieChart, Pie, ResponsiveContainer, Cell } from "recharts";
+import { PieChart, Pie, ResponsiveContainer, Cell, Tooltip } from "recharts";
 
 export const Profile = () => {
   const [finalData, setFinalData] = useState([
@@ -15,33 +15,6 @@ export const Profile = () => {
   useEffect(() => {
     console.log("in useEffect");
     fetchData();
-    // fetch("/postQuestionnaire/getUserResults")
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log("data: ", data);
-    //     const pieChartData = data.results[0];
-    //     console.log("piechartdata: ", pieChartData);
-    //     const workingData = [];
-    //     workingData.push({
-    //       name: "mobility_kg",
-    //       value: Number(pieChartData.mobility_kg).toFixed(0)
-    //     });
-    //     workingData.push({
-    //       name: "consumption_kg",
-    //       value: Number(pieChartData.consumption_kg).toFixed(0),
-    //     });
-    //     workingData.push({
-    //       name: "household_kg",
-    //       value: Number(pieChartData.household_kg).toFixed(0),
-    //     });
-    //     workingData.push({
-    //       name: "home_kg",
-    //       value: Number(pieChartData.household_kg).toFixed(0),
-    //     });
-    //     console.log("working data line 57", workingData);
-    //     setFinalData(workingData);
-    //     // render()
-    //   });
   }, []);
 
   const fetchData = () => {
@@ -69,8 +42,6 @@ export const Profile = () => {
         // render()
       });
   }
-
-  console.log(typeof finalData[0].value);
 
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
@@ -100,30 +71,55 @@ export const Profile = () => {
       </text>
     );
   };
+  const getIntroOfPage = (label) => {
+    if (label == 'mobility_kg') {
+      return "You move too much";
+    }
+    if (label == 'consumption_kg') {
+      return "You eat too much";
+    }
+    if (label == 'household_kg') {
+      return "Your house hates the Earth";
+    }
+    return 'be better!';
+  };
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      console.log('payload: ',payload)
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`${payload[0].name} : ${payload[0].value}kg`}</p>
+          <p className="intro">{getIntroOfPage(label)}</p>
+          <p className="desc">Mother Earth is dyin' ova here.</p>
+        </div>
+      );
+    }
+  
+    return null;
+  };
   return (
-    <div>
+    <div id='profile-component'>
       <header>
         <h1 id="tableTitle">Your Footprint</h1>
       </header>
-      {/* <ResponsiveContainer width="100%" height="100%"> */}
-        <PieChart width={900} height={900}>
-          <Pie
-              data={finalData}
-              cx="50%"
-              cy="50%"
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-              >
-            {finalData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-          </Pie>
-        </PieChart>
-      {/* </ResponsiveContainer> */}
-      {/* {finalData  || 'hello'} */}
+      <PieChart width={400} height={400} id='pie-chart'>
+        <Pie
+            data={finalData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={200}
+            fill="#8884d8"
+            dataKey="value"
+            >
+          {finalData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+        </Pie>
+        <Tooltip id='custom-tooltip' content={<CustomTooltip />} />
+
+      </PieChart>
     </div>
   );
 };
