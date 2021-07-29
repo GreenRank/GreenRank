3f4c551603a06e228cd48483fa4cea3725d48c98
Raubern Totanes <rstotanes@g.ucla.edu>
2021-07-28 21:33:11 -0400

    built out about component

--- client/About.jsx
+++ client/About.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 export const About = () => {
-  let count;
+  let count = 0;
+  const history = useHistory();
 
-  fetch()
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      count = data.count;
-    });
+  // fetch()
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     count = data.count;
+  //   });
+
+  const goBack = () => history.push('/Home')
+
   return (
-    <div>
-      <p>Four cool guys just trying to save the world...</p>
-      <button onClick={count++}>
-        Join us oun our journey to reduce carbon emissions.
+    <div id='about'>
+      <button onClick={goBack} id='go-back'>go back</button>
+      <img src='https://lh3.googleusercontent.com/proxy/ZR9JToynrdlcEQ1medMETPVnOea6BQqd-rLUxI1QOguJ2GVhHHArSI_gQRmLPXGTH0neeDFzIQx_B5A9cbjgPXLh0QYsCCOge0fZV-LckCAGme4EvohnSNwARAy79KzcPjEuIsY3kSjgP5uMX0BwTpp5pPSTyU-iGgeMS6fa1w' alt='clowns' id='about-img'></img>
+      <div className='about-desc'>Four cool guys just trying to save the world...</div>
+      <div id='about-count'>{ count }</div>
+      <button onClick={count++} className='about-desc'>
+        Join us on our journey to reduce carbon emissions
       </button>
-      <p>{{ count }}</p>
     </div>
   );
 };
